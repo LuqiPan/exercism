5ed39e7284ff59199a144c815f67a2c882e3b029
Luqi Pan <luqi.pan@gmail.com>
2014-08-25 16:23:31 -0400

    Added a valid check so it will throw error when it's not right

--- clojure/nucleotide-count/nucleotide_count.clj
+++ clojure/nucleotide-count/nucleotide_count.clj
@@ -7,6 +7,12 @@
           {\A 0, \T 0, \C 0, \G 0}
           nuc-seq))
 
+(defn valid-nucleotide?
+  [nucleotide]
+  (#{\A \T \C \G \U} nucleotide))
+
 (defn count
   [nucleotide nuc-seq]
+  (if-not (valid-nucleotide? nucleotide)
+    (throw (Exception. "invalid nucleotide")))
   (get (nucleotide-counts nuc-seq) nucleotide 0))
